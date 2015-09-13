6b5c17e27e4658c7b6f8a692c5d919ae2ed8fe35
hannerasa <hannele.rasanen@helsinki.fi>
2015-09-13 18:38:50 +0300

    Viikko 2

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,10 +1,12 @@
-<?php
+﻿<?php
 
   class HelloWorldController extends BaseController{
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	 // View::make('home.html');
+         echo 'Tämä on etusivu!';
+
     }
 
     public static function sandbox(){
