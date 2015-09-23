202ab7fea7f72036be92aa73ab0191d271bfe9d8
hannerasa <hannele.rasanen@helsinki.fi>
2015-09-23 08:24:12 +0300

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,15 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+    
+    $routes->get('/astia', function(){
+  AstiatController::listaa();
+   });
+   
+  $routes->post('/astia', function(){
+  AstiatController::store();
+});
+   // Astian lisäyslomakkeen näyttäminen
+$routes->get('/lisays/new', function(){
+  AstiatController::create();
+});
